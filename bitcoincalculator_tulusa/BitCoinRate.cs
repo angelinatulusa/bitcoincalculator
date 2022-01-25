420b69831781303eb081801fec396ced31ac1bdf
Opilane TTHK <opilane@tthk.ee>
2022-01-25 13:31:34 +0200

    bitcoin

---
+++ bitcoincalculator_tulusa/BitCoinRate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bitcoincalculator_tulusa
+{
+    class BitCoinRate
+    {
+        public bpi bpi { get; set; }
+    }
+
+    public class bpi
+    {
+        public USD USD { get; set; }
+    }
+    public class USD
+    {
+        public string code { get; set;  }
+
+        public float rate_float { get; set; }
+    }
+
+}
---
+++ bitcoincalculator_tulusa/Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace bitcoincalculator_tulusa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitCoinRate bitcoin = GetRates();
+
+            Console.WriteLine($"Current rate in {bitcoin.bpi.USD.code}: {bitcoin.bpi.USD.rate_float}");
+
+
+        }
+
+
+        public static BitCoinRate GetRates()
+        {
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "Get";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            BitCoinRate bitcoin;
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                bitcoin = JsonConvert.DeserializeObject<BitCoinRate>(response);
+            }
+
+            return bitcoin;
+        }
+    }
+}
